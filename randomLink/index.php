c993c10d1f4c94e371623b486d2ac5e129a927e6
nescafever <mehmetcanyarar34@gmail.com>
2021-07-17 03:02:31 +0300

    Random Link Generator
    php random link generator

---
+++ randomLink/index.php
@@ -0,0 +1,28 @@
+<?php
+function randomURL($URLlength = 8) { // random maksimum 8 karakterli bir değer oluşturup bunu fonksiyon olarak kaydediyoruz.
+    $charray = array_merge(range('a','z'), range('0','9'));
+    $max = count($charray) - 1;
+    for ($i = 0; $i < $URLlength; $i++) {
+        $randomChar = mt_rand(0, $max);
+        $url .= $charray[$randomChar];
+    }
+    return $url;
+}
+
+
+$dosya = randomURL() . ".php"; // bu projede dosyayı php olarak kaydetmek istediğim için, randomURL() fonksiyon arkasına ".php" ekliyorum ve dosya değişkenine yazıyorum.
+echo $dosya;
+
+
+$verigeldimi = $_POST[buton]; //butona tıklandığında çalışmasını istediğim için $verigeldimi değişkenine butonu post ediyorum.
+
+if(isset($verigeldimi)=="cagir"){ // eğer cagir elemanından veri geldiyse dosyayı oluşturup ekrana yazdırıyorum.
+fopen($dosya, "w");
+echo "<br /> DOSYA BAŞARILYA OLUŞTURULDU. <br /> Oluşturulan Yol:" . "<a>https://mehmetcanyarar.com.tr/projects/randomLink/" . $dosya . "</a>";
+die();
+}
+
+?>
+<form method="POST">
+<input type="submit" name="buton" value="cagir">
+</form>
